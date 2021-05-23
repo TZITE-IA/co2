987ffdb5b8df9e7b05a26b552cda8cc61a20f166
JuanDanielSoto <halogeno321@gmail.com>
2021-05-22 23:58:03 -0500

    automatic

--- app/main.py
+++ app/main.py
@@ -1,88 +1,61 @@
 from flask import Flask, request, jsonify
-import json
+from sharedTools import *
 
 app= Flask(__name__)
-
-class database(object):
-    def __init__(self, file):
-        self.file = file
-        
-    def load(self):
-        with open('app/db.json') as json_file:
-            db = json.load(json_file)
-            return db
-    
-    def update(self, db):
-        with open('app/db.json', 'w') as outfile:
-            json.dump(db, outfile)
-
 DB = database("db.json")
 
 @app.route("/")
 def all():
       db = DB.load()
-      print([s["label"] for s in db])
       return jsonify(db)
     
 @app.route("/<string:sen>")
 def sensor(sen):
       for s in DB.load():
-            if sen == s["label"]:
+            if sen == s["name"]:
                   return jsonify(s)
 
-@app.route("/new", methods=["POST"])
-def postNewSensor():
-    post = request.json
-    db = DB.load()
-    exist = ["True" for s in db if post["label"] == s["label"]]
-    if len(exist) > 0:
-          return jsonify({"error": "'"+post["label"]+"' alrredy exists"})
-    else:
-          db.append(post)
-          DB.update(db)
-          return jsonify(DB.load())
-    # products.append(nuevo)
-    # return jsonify({"message":"Añadido correctamente","products": products})
+@app.route("/new/<string:sen>", methods=["POST"])
+def postNewSensor(sen):
+      db = DB.load()
+      exist = ["True" for i, s in enumerate(db) if sen == s["name"]]
+      if len(exist) > 0:
+            return jsonify({"error": "'"+sen+"' alrredy exists"})
+      else:
+            db.append({"name":sen,"series":[ {"value" : 0,"name": "0"}]})
+            DB.update(db)
+            return jsonify({"sucess": "'"+sen+"' fue creado correctamente"})
 
+@app.route("/update", methods=["PUT"])
+def updateValues():
+      """
+      request - 
+      {
+        "name": "nombre",
+        "series": {
+          "value": number,
+          "name": string
+          }
+      }
+      """
+      post = request.json
+      db = DB.load()
+      exist = [i for i, s in enumerate(db) if post["name"] == s["name"]]
+      if len(exist) == 0:
+            return jsonify({"error": "'"+post["name"]+"' does not exists"})
+      else:
+            db[exist[0]]["series"].append(post["series"])
+            DB.update(db)
+            return jsonify({"sucess": "'"+post["name"]+"' fue actualizado correctamente"})
 
-
-
-
-
-@app.route("/products/<string:producto>", methods=["PUT"])
-def editProduct(producto):
-    res = []
-    for item in products:
-        if producto == item["name"]:
-            res.append(item)
-    if len(res) > 0:
-        res[0]["name"] =  request.json["name"]
-        res[0]["price"] = request.json["price"]
-        res[0]["quantity"] = request.json["quantity"]
-        return jsonify({"result":"Hecho","producto actualizado":res[0]})
-    else:
-        return jsonify({"result":"Producto no encontrado"})
-
-@app.route("/products/<string:producto>", methods=["DELETE"])
-def deleteProduct(producto):
-    res = []
-    for item in products:
-        if producto == item["name"]:
-            res.append(item)
-    if len(res) > 0:
-        products.remove(res)
-        return jsonify({"result":"Eliminado","producto eliminado": res})
-    else:
-        return jsonify({"result":"Producto no encontrado"})
-      
-@app.route("/products")
-def getProducts():
-    return jsonify(products)
-
-@app.route("/products/<string:producto>")
-def getProduct(producto):
-    res = []
-    for item in products:
-        if producto == item["name"]:
-            res.append(item)
-    return jsonify(res)
+@app.route("/delete/<string:sen>", methods=["DELETE"])
+def deleteSensor(sen):
+      db = DB.load()
+      exist = [i for i, s in enumerate(db) if sen == s["name"]]
+      if len(exist) == 0:
+            return jsonify({"error": "'"+sen+"' does not exists"})
+      else:
+            db.pop(exist[0])
+            DB.update(db)
+            return jsonify({"sucess": "'"+sen+"' was deleted"})
+  
---
+++ sharedTools.py
@@ -0,0 +1,16 @@
+import json
+
+class database(object):
+    def __init__(self, file):
+        self.file = file
+        
+    def load(self):
+        with open('app/db.json') as json_file:
+            db = json.load(json_file)
+            json_file.close()
+            return db
+    
+    def update(self, db):
+        with open('app/db.json', 'w') as outfile:
+            json.dump(db, outfile)
+            outfile.close()
