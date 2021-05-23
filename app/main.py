c7f63cc03dc0bab72443838b6973e8de800ae28e
JuanDanielSoto <halogeno321@gmail.com>
2021-05-23 04:18:20 -0500

    automatic

--- app/main.py
+++ app/main.py
@@ -26,11 +26,11 @@ def postNewSensor(sen):
       db = DB.load()
       exist = ["True" for i, s in enumerate(db) if sen == s["name"]]
       if len(exist) > 0:
-            return jsonify({"error": "'"+sen+"' alrredy exists"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"error": "'"+sen+"' alrredy exists"})
       else:
             db.append({"name":sen,"series":[ {"value" : 0,"name": "0"}]})
             DB.update(db)
-            return jsonify({"sucess": "'"+sen+"' fue creado correctamente"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"sucess": "'"+sen+"' fue creado correctamente"})
 
 @app.route("/update", methods=["PUT"])
 def updateValues():
@@ -48,20 +48,20 @@ def updateValues():
       db = DB.load()
       exist = [i for i, s in enumerate(db) if post["name"] == s["name"]]
       if len(exist) == 0:
-            return jsonify({"error": "'"+post["name"]+"' does not exists"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"error": "'"+post["name"]+"' does not exists"})
       else:
             db[exist[0]]["series"].append(post["series"])
             DB.update(db)
-            return jsonify({"sucess": "'"+post["name"]+"' fue actualizado correctamente"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"sucess": "'"+post["name"]+"' fue actualizado correctamente"})
 
 @app.route("/delete/<string:sen>", methods=["DELETE"])
 def deleteSensor(sen):
       db = DB.load()
       exist = [i for i, s in enumerate(db) if sen == s["name"]]
       if len(exist) == 0:
-            return jsonify({"error": "'"+sen+"' does not exists"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"error": "'"+sen+"' does not exists"})
       else:
             db.pop(exist[0])
             DB.update(db)
-            return jsonify({"sucess": "'"+sen+"' was deleted"}).headers.add("Access-Control-Allow-Origin", "*")
+            return jsonify({"sucess": "'"+sen+"' was deleted"})
   
