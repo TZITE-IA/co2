b72f49b8b8e230afb99c59b4c736898c29ff3493
JuanDanielSoto <halogeno321@gmail.com>
2021-05-27 01:31:42 -0500

    automatic

--- app/main.py
+++ app/main.py
@@ -1,75 +1,91 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, render_template, flash
 from datetime import datetime
 from sharedTools import *
 from flask_cors import CORS
-import math
 
 app= Flask(__name__)
 DB = database("db")
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
-#cors = CORS(app, resources={r"/*":{"origins": "*"}})
 
-@app.route("/math")
-def numpy():
-      return jsonify({"result": str(math.log(1))})
-
-@app.route("/")
+@app.route("/") #actualizado
 def all():
-      db = DB.load()
+      return jsonify(DB.load())
+
+@app.route("/in") #actualizado
+def allIn():
+      db = []
+      for i, d in enumerate(DB.load()):
+            db.append({"name":d["name"], "series":[]})
+            for j, b in enumerate(d["params"]):
+                  for series in b["series"]:
+                        db[i]["series"].append(series)
+                  j+=1
+            i+=1
+      return jsonify(db)
+
+@app.route("/out") #actualizado
+def allOut():
+      db = []
+      for i, d in enumerate(DB.load()):
+            db.append({"name":d["name"], "series":[]})
+            for j, b in enumerate([d["params"][1]]):
+                  db[i]["series"].append(b["series"])
+                  j+=1
+            i+=1
       return jsonify(db)
     
-@app.route("/<string:sen>")
+@app.route("/<string:sen>") #actualizado
 def sensor(sen):
       for s in DB.load():
             if sen == s["name"]:
                   return jsonify(s)
+      return jsonify({"error":"'"+sen+"' not exist"})
             
 @app.route("/last/<string:val>")
-def lastVal(val):
+def lastVal(val): #actualizado
       try:
             val = int(val)
       except Exception:
             return jsonify({"error": val +" no es una entrada valida"})
       db = []
       for i, d in enumerate(DB.load()):
-            db.append({"name":d["name"], "series":[]})
-            for series in d["series"][::-1][:int(val)]:
-                  db[i]["series"].append(series)
-            db[i]["series"] = db[i]["series"][::-1]
-            i+=1;
+            db.append({"name":d["name"], "params":[{"name":"Adentro","series":[]},{"name":"Afuera","series":[]}]})
+            for j, b in enumerate(d["params"]):
+                  for series in b["series"][::-1][:int(val)]:
+                        db[i]["params"][j]["series"].append(series)
+                  j+=1
+            i+=1
       return jsonify(db)
 
-@app.route("/new/<string:sen>")
+@app.route("/new/<string:sen>") #actualizado
 def postNewSensor(sen):
       db = DB.load()
-      
-      exist = ["True" for i, s in enumerate(db) if sen == s["name"]]
-      if len(exist) > 0:
+      if exist(sen, db):
             return jsonify({"error": "'"+sen+"' alrredy exists"})
       else:
-            db.append({"name":sen,"series":[]})
+            db.append({"name":sen,"params": [{"name": "Adentro", "series": []},{"name": "Afuera", "series": []}]})
             DB.update(db)
             return jsonify({"sucess": "'"+sen+"' fue creado correctamente"})
       
-@app.route("/clear/<string:sen>")
+@app.route("/clear/<string:sen>") #actualizado
 def clearValues(sen):
       db = DB.load()
       exist = [i for i, s in enumerate(db) if sen == s["name"]]
       if len(exist) == 0:
             return jsonify({"error": "'"+sen+"' does not exists"})
       else:
-            now = datetime.now()
-            db[exist[0]]["series"]= []
+            db[exist[0]]["params"]= [{"name":"Adentro","series":[]},{"name":"Afuera","series":[]}]
             DB.update(db)
             return jsonify({"sucess": "'"+sen+"' fue limpiado correctamente"})
 
-@app.route("/update", methods=["PUT"])
+@app.route("/update", methods=["PUT"]) #actualizado
 def updateValues():
       """
       request - 
       {
         "name": "nombre",
+        "site": "ubicación"
         "series": {
           "value": number,
           "name": string
@@ -77,20 +93,22 @@ def updateValues():
       }
       """
       post = request.json
+      
       db = DB.load()
       exist = [i for i, s in enumerate(db) if post["name"] == s["name"]]
       if len(exist) == 0:
             return jsonify({"error": "'"+post["name"]+"' does not exists"})
       else:
             now = datetime.now()
-            db[exist[0]]["series"].append({"value": post["series"]["value"], "name":str(now.date())+"/"+str(now.time())})
+            for i, ub in enumerate(db[exist[0]]["params"]):
+                  if ub["name"] == post["site"]:
+                        db[exist[0]]["params"][i]["series"].append({"value": post["series"]["value"], "name":str(now.date())+"/"+str(now.time())})
             DB.update(db)
             return jsonify({"sucess": "'"+post["name"]+"' fue actualizado correctamente"})
       
 
-
 @app.route("/delete/<string:sen>", methods=["DELETE"])
-def deleteSensor(sen):
+def deleteSensor(sen): #actualizado
       db = DB.load()
       exist = [i for i, s in enumerate(db) if sen == s["name"]]
       if len(exist) == 0:
@@ -99,4 +117,18 @@ def deleteSensor(sen):
             db.pop(exist[0])
             DB.update(db)
             return jsonify({"sucess": "'"+sen+"' was deleted"})
-  
+
+@app.route("/ach")
+def ach():
+      temp = []
+      for sens in DB.load():
+            try:   
+                  inicio = sens["params"][0]["series"][::-1][1]
+                  final = sens["params"][0]["series"][::-1][0]
+                  afuera_i = sens["params"][1]["series"][::-1][1]
+                  afuera_f = sens["params"][1]["series"][::-1][0]
+                  temp.append({"name":sens["name"]+"-ACH", "ACH": ACH(inicio, final, afuera_i, afuera_f) , "time": sens["params"][0]["series"][::-1][0]["name"]})
+            except Exception:
+                  temp.append({"name":sens["name"]+"-ACH", "ACH": "Faltan datos" , "time": "Sin fecha"})
+      return jsonify(temp)
+      
--- sharedTools.py
+++ sharedTools.py
@@ -1,16 +1,67 @@
 import json
+import math
+
 
 class database(object):
     def __init__(self, file):
         self.file = file+".json"
         
     def load(self) :  
-        with open('app/db.json') as json_file:
+        with open('app/'+self.file) as json_file:
             db = json.load(json_file)
             json_file.close()
             return db
     
     def update(self, db):
-        with open('app/db.json', 'w') as outfile:
+        with open('app/'+self.file, 'w') as outfile:
             json.dump(db, outfile)
             outfile.close()
+
+def exist(name, db):
+    """
+    Returns True if exists the name else False
+    """
+    exist = ["True" for i, s in enumerate(db) if name == s["name"]]
+    if len(exist) > 0:
+        return True
+    else:
+        return False
+    
+def ACH(inicio, final, afuera_i, afuera_f):
+    """
+    inicio - {CO2 adendtro t1, time}
+    final -  {CO2 adentro t2, time}
+    afuera_i {CO2 afuera t1, time}
+    afuera_f {CO2 afuera t2, time}
+    
+    @returns ACH
+    """
+    ## separar la fecha de la hora ##########
+    tiempo1 = inicio['name'].split('/')[1]
+    tiempo1 = tiempo1.split(':')
+    tiempo1 = [float(i) for i in tiempo1]
+    
+    tiempo2 = final['name'].split('/')[1]
+    tiempo2 = tiempo2.split(':')
+    tiempo2 = [float(i) for i in tiempo2]
+    
+    ## minutos a horas ######################
+    tiempo1[1] = tiempo1[1] / 60
+    tiempo2[1] = tiempo2[1] / 60
+    ## segundos a horas #####################
+    tiempo1[2] = tiempo1[2] / 3600
+    tiempo2[2] = tiempo2[2] / 3600
+    
+    tiempo1 = tiempo1[0] + tiempo1[1] + tiempo1[2]
+    tiempo2 = tiempo2[0] + tiempo2[1] + tiempo2[2]
+    
+    ## calculo de ACH #######################
+    C_amb = afuera_f['value'] - afuera_i['value'] 
+    C_amb = C_amb / 2
+    C_start = inicio['value']
+    C_end = final['value']
+    x = (C_end - C_amb) / (C_start - C_amb)
+    x = -1 * math.log(x)
+    x = x / (tiempo2 - tiempo1)
+    
+    return x
